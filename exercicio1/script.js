bec16100d063a32a97803420d971e13bde74dc8f
gaaveloso <gabrielvelosorezende-po@hotmail.com>
2022-09-08 12:04:46 -0300

    exercicios aula a013 resolvidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,18 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número:"))
 
+//Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+if(num%2===0){
+    console.log("O número digitado é divisivel por 2")
+    if(num%3===0){
+        console.log("O número digitado é divisivel também por 3")
+    } else {
+        console.log("O número digitado não é divisivel por 3")
+    }
+} else {
+    if(num%3===0){
+        console.log("O numero digitado é divisivel por 3")
+    } else {
+        console.log("O numero não é divisivel por 3")
+    } 
+    console.log("O número não é divisivel por 2")
+}
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,21 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+switch(nacionalidade){
+    case "brasileira":
+        console.log("Você é brasileira.")
+        break
+    case "argentina":
+        console.log("Você é argentina")
+        break
+    case "uruguaia":
+        console.log("Você é uruguaia")
+        break
+    case "chilena":
+        console.log("Você é chilena")
+        break
+    case "colombiana":
+        console.log("Você é colombiana")
+        break
+    default:
+        console.log("Nacionalidade não encontrada")
 }
